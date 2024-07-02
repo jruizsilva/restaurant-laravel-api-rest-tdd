b550cb47a1e82560543724a96f1f8e0dd075790b
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2024-07-02 18:23:18 -0300

    edit plate tests added

--- tests/Feature/Plate/EditPlateTest.php
+++ tests/Feature/Plate/EditPlateTest.php
@@ -58,8 +58,89 @@ class EditPlateTest extends TestCase
             ]),
             $data
         );
+        $responseData = $response->json('data');
 
         $response->assertStatus(200);
+        $response->assertJsonPath('data', [
+            ...$responseData,
+            ...$data
+        ]);
         $this->assertDatabaseHas('plates', $data);
+    }
+
+    #[Test]
+    public function an_authenticated_user_cannot_edit_a_plate_that_does_not_belong_to_his_restaurant(): void
+    {
+        $data = [
+            'name' => 'Plate 2 edited',
+            'description' => 'Description 2 edited',
+            'price' => 13.99
+        ];
+        $response = $this->actingAs($this->user)->putJson(
+            route('restaurant.plates.update', [
+                'restaurant' => $this->anotherRestaurant->id,
+                'plate' => $this->anotherPlate->id
+            ]),
+            $data
+        );
+        $response->assertStatus(401);
+        $response->assertJsonPath('status', 401);
+    }
+
+    #[Test]
+    public function an_unauthenticated_user_cannot_edit_a_plate(): void
+    {
+        $data = [
+            'name' => 'Plate 1 edited',
+            'description' => 'Description 1 edited',
+            'price' => 11.99
+        ];
+        $response = $this->putJson(
+            route('restaurant.plates.update', [
+                'restaurant' => $this->restaurant->id,
+                'plate' => $this->plate->id
+            ]),
+            $data
+        );
+        $response->assertStatus(401);
+        $response->assertJsonPath('status', 401);
+    }
+
+    #[Test]
+    public function an_authenticated_user_cannot_edit_a_plate_that_does_not_exist(): void
+    {
+        $data = [
+            'name' => 'Plate 1 edited',
+            'description' => 'Description 1 edited',
+            'price' => 11.99
+        ];
+        $response = $this->actingAs($this->user)->putJson(
+            route('restaurant.plates.update', [
+                'restaurant' => $this->restaurant->id,
+                'plate' => 999
+            ]),
+            $data
+        );
+        $response->assertStatus(404);
+        $response->assertJsonPath('status', 404);
+    }
+
+    #[Test]
+    public function an_authenticated_user_cannot_edit_a_plate_of_a_restaurant_that_does_not_exist()
+    {
+        $data = [
+            'name' => 'Plate 1 edited',
+            'description' => 'Description 1 edited',
+            'price' => 11.99
+        ];
+        $response = $this->actingAs($this->user)->putJson(
+            route('restaurant.plates.update', [
+                'restaurant' => 999,
+                'plate' => $this->plate->id
+            ]),
+            $data
+        );
+        $response->assertStatus(404);
+        $response->assertJsonPath('status', 404);
     }
 }
